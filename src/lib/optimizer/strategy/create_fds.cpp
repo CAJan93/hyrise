b6637056e32060892a713fe791c0cbecdabd42d4
Julian Menzler <julianmenzler@gmail.com>
2020-08-26 15:21:58 +0200

    Reduce iterations to 1

--- src/lib/optimizer/strategy/create_fds.cpp
+++ src/lib/optimizer/strategy/create_fds.cpp
@@ -6,15 +6,11 @@
 namespace opossum {
 
 void CreateFDsRule::apply_to(const std::shared_ptr<AbstractLQPNode>& lqp) const {
-
-  for(size_t i = 0; i < 100; ++i) {
     Assert(!lqp->right_input(), "Did not expect right input node");
     if(!dynamic_pointer_cast<AbstractNonQueryNode>(lqp->left_input())) {
       const auto& fds = lqp->left_input()->functional_dependencies();
       Assert(fds.size() > 0 || fds.size() == 0, "Dummy usage");
     }
-  }
-
 }
 
 }  // namespace opossum
--- src/lib/optimizer/strategy/create_unique_constraints.cpp
+++ src/lib/optimizer/strategy/create_unique_constraints.cpp
@@ -6,15 +6,11 @@
 namespace opossum {
 
 void CreateUniqueConstraintsRule::apply_to(const std::shared_ptr<AbstractLQPNode>& lqp) const {
-
-  for(size_t i = 0; i < 100; ++i) {
     Assert(!lqp->right_input(), "Did not expect right input node");
     if(!dynamic_pointer_cast<AbstractNonQueryNode>(lqp->left_input())) {
       const auto& unique_constraints = lqp->left_input()->unique_constraints();
       Assert(unique_constraints->size() > 0 || unique_constraints->size() == 0, "Dummy usage");
     }
-  }
-
 }
 
 }  // namespace opossum
